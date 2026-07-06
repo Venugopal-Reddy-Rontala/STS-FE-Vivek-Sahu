b4272ff0a5e8ed95c867872fea349f48d598f395
Vivek Sahu <sahuv041@gmail.com>
2026-07-06 15:52:25 +0530

    submisiion of task 2

---
+++ frontend/tests/test2/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import CountryCard from './components/countryCard.vue';
+</script>
+
+<template>
+  <CountryCard> </CountryCard>
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/tests/test2/src/components/countryCard.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+import { ref, onMounted } from 'vue';
+
+const countries = ref({});
+
+async function getCountry(){
+    try {
+        
+        let res = await fetch(
+            'https://api.restcountries.com/countries/v5?q=canada',
+            { headers: { 'Authorization': 'Bearer rc_live_ca5fa02c47f647ee9a088cad843c054d' } }
+            )
+
+        const data = await res.json();
+
+        console.log(data.object)
+
+        countries.value = data
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+onMounted(()=>{
+    getCountry();
+})
+
+
+</script>
+
+<template>
+    <h1>country card</h1>
+
+    <!-- <pre>
+        {{ countries }}
+    </pre> -->
+
+
+
+    <div v-for="country in countries" >
+        {{ country  }}
+
+
+    </div>
+
+</template>
+
+<style>
+
+</style>
